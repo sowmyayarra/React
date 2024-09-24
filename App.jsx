d307e966477e78cccd4c80c2d4b9fdddc65f02b8
sowmyayarra <147241169+sowmyayarra@users.noreply.github.com>
2024-09-24 10:30:58 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import react from  'react'
+import HelloWorld from './HelloWorld'
+function App()
+{
+    return(
+        <div>
+        <h1>Hello Welcome to React</h1>
+        </div>
+
+    )
+}
+export default App
---
+++ HelloWorld.jsx
@@ -0,0 +1,11 @@
+import react from  "react"
+function HelloWorld()
+{
+    return(
+        <div>
+        <h1>Hello World</h1>
+        </div>
+
+    )
+}
+export default HelloWorld;
